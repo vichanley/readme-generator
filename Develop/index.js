c2c0f34a33387354d9c1bb04da2729d04ec3c23f
vichanley <viccantu23@gmail.com>
2022-03-04 14:36:14 -0600

    generating markdown

--- Develop/index.js
+++ Develop/index.js
@@ -35,14 +35,14 @@ const userQuestions = [
       },
       {
         type: 'checkbox',
-        name: 'licenses',
+        name: 'license',
         message: 'Which license would you like to include for this project?',
         choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense']
       },
       {
         type: 'input',
         name: 'contributors',
-        message: 'List your collaborators, if any.',
+        message: 'List your contributors, if any.',
       },
       {
         type: 'input',
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -3,23 +3,68 @@
 function renderLicenseBadge(license) {
   if(license) {
     const badge = license.map(element => {
-      return 
-    })
+      return `![alt text](https://img.shields.io/static/v1?label=licence&message=${encodeURIComponent(element)}&color=GREEN)`;
+    });
+
+    return badge.join(' ');
+  } else {
+    return '';
   }
-}
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license){
+    return '[Link to License] (../src/' + encodeURIComponent(license) + '.txt)'
+  } else {return ''};
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license){
+    return '[License info] (../src/' + encodeURIComponent(license) + '.txt)'
+  } else {return ''};
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ${renderLicenseBadge(data.license)}
+
+  ## Description 
+  ${data.description}
+
+  ## Table of Contents 
+  *[Installation](#Installation)
+  *[Usage](#Usage)
+  *[License](#License)
+  *[Contributing](#Contributing)
+  *[Tests](#Tests)
+  *[Questions](#Questions)
+
+  ## Installation 
+  ${data.installation}
+
+  ## Usage 
+  ${data.usage}
+
+  ## License 
+  ![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)
+  <br />
+  This application is covered by the ${data.license} license. 
+
+  ## Contributing 
+  ${data.contributors}
+
+  ## Tests 
+  ${data.tests}
+  
+  ## Questions 
+  You can find me on GitHub: [${data.username}] (https://github.com/${data.username})
+  You can also e-mail me with any questions at ${data.email} .
 `;
 }
 
