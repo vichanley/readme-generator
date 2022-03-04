c392493041a2ca36016fab383571f089de54c033
vichanley <viccantu23@gmail.com>
2022-03-04 14:05:57 -0600

    Adding questions and generating readme file

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,86 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const userQuestions = [
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?',
+        validate: titleInput => {
+          if (titleInput) {
+            return true;
+          } else {
+            console.log('Please enter your project title!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a short description explaining the what, why, and how of your project. &#013; Use the following questions as a guide: &#013; -What was your motivation? &#013; -Why did you build this project? &#013; -What problem does it solve? &#013; - What did you learn?',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide examples for use.',
+      },
+      {
+        type: 'checkbox',
+        name: 'licenses',
+        message: 'Which license would you like to include for this project?',
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense']
+      },
+      {
+        type: 'input',
+        name: 'contributors',
+        message: 'List your collaborators, if any.',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'Go the extra mile and write tests for your applications.',
+      },
+      {
+        type: 'input',
+        name: 'username',
+        message: 'Please provide your github username.',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Please provide your e-mail address.',
+      },
+    ];
+  
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    readMe = generateMarkdown(data);
+
+    fs.writeFile('README.md', generatePage(), err => {
+        if (err) throw err;
+  
+        console.log('README.md creation complete!');
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
-
+function init() {
+    return inquirer.prompt(userQuestions)
+        .then(userAnswersData => {
+            writeToFile('./dist/README.md', userAnswersData);
+        }).catch(err => {
+            console.log(err);
+        });
+}
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,12 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license) {
+    const badge = license.map(element => {
+      return 
+    })
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -18,3 +24,41 @@ function generateMarkdown(data) {
 }
 
 module.exports = generateMarkdown;
+
+const fs = require('fs');
+
+// writing files
+const writeFile = fileContent => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile('./dist/index.html', fileContent, err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve({
+        ok: true,
+        message: 'File created!'
+      });
+    });
+  });
+};
+
+// copying file
+const copyFile = () => {
+  return new Promise((resolve, reject) => {
+    fs.copyFile('./src/style.css', './dist/style.css', err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve({
+        ok: true,
+        message: 'Stylesheet created!'
+      });
+    });
+  });
+};
+
+module.exports = { writeFile, copyFile };
